886019da6071c0e40b0ce4e72f951ccc533963f8
Adolfovi <32370254+Adolfovi@users.noreply.github.com>
2021-07-05 13:25:57 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -43,7 +43,7 @@ function ruletone() {
 
 // FUNCIÓN QUE ACTIVA EL EFECTO RULETA
 function ruletoneactived() {
-    rone = setInterval('ruletone()', 125);
+    rone = setInterval('ruletone()', 100);
 
 }
 
@@ -68,7 +68,7 @@ function rulettwo() {
 
 // FUNCIÓN QUE ACTIVA EL EFECTO RULETA
 function rulettwoactived() {
-    rtwo = setInterval('rulettwo()', 125);
+    rtwo = setInterval('rulettwo()', 100);
 
 }
 
@@ -95,7 +95,7 @@ function ruletthree() {
 
 // FUNCIÓN QUE ACTIVA EL EFECTO RULETA
 function ruletthreeactived() {
-    rthree = setInterval('ruletthree()', 125);
+    rthree = setInterval('ruletthree()', 100);
 
 }
 
